427d10a65a2a1e3c2b738875deaafd312239c6a2
WHKL-9 <64627811+WHKL-9@users.noreply.github.com>
2021-12-03 14:46:29 +0100

    Update function.js

--- src/utilities/function.js
+++ src/utilities/function.js
@@ -1,15 +1,3 @@
-export const fetchTransaction = async (endpoint) => {
-  try {
-    const response = await fetch(endpoint);
-    if (response.ok) {
-      const data = await response.json();
-      console.log(data)
-      return data;
-    }
-  } catch (error) {
-    console.log(error);
-  }
-};
 
 export const blockTransaction = async (id, endpoint, setLoading) => {
   try {
